f565774a068ba6a281b139d62641f2d2ed612abf
David Cingala <david.cingala@gmail.com>
2015-07-02 21:26:41 +0200

    little updates

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
+var http = require('http');
 var BinaryServer = require('binaryjs').BinaryServer;
-var server = BinaryServer({port: 9000});
-var fs = require('fs');
+var express = require('express');
 var _ = require('lodash');
 
-server.on('connection', function(client){
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+var server = http.createServer(app);
+
+var bs = BinaryServer({server: server});
+
+bs.on('connection', function(client){
     client.on('stream', function(stream, meta){
 
-        var clients = _.values(server.clients);
+        var clients = _.values(bs.clients);
         var otherClients = clients.filter(function(cl){return client !== cl});
 
         _.each(otherClients, function(other){
             var responseStream = other.createStream(meta);
             stream.pipe(responseStream);
         });
-
     });
+});
+
+server.listen(9000, function(){
+    console.log('server listen port 9000');
 });
